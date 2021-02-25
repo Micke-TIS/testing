3e490c00ed1182960e37d3af7e7cbdf37729ef7a
Micke <mikael.johanssonj@gmail.om>
2021-02-25 15:50:14 +0100

    ffff

--- includes/config.php
+++ includes/config.php
@@ -11,18 +11,8 @@ if(!$conn){
 }
 
 
-
-
-
-
-
-
-
-
-
-
 //Select specific columns from DB
-$query = mysqli_query($sql, "SELECT * FROM tbl_articles");
+$conn = mysqli_query($sql, "SELECT * FROM tbl_articles");
 while($row = mysqli_fetch_assoc($query))
 {
   $title = $row['title'];
