8c9301babf35b6640d59ad9a843d20535ff4212d
Micke <mikael.johanssonj@gmail.om>
2021-02-25 15:23:44 +0100

    bbbb

---
+++ includes/config.php
@@ -0,0 +1,12 @@
+?<?php
+//Database connection
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "testingSQL";
+
+$sql = mysqli_connect($host, $username, $password, $database) or die('Could not connect');
+
+
+ ?>
