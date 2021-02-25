cf4788659b5dfa855439e9ef17c7872a0eeb0e22
Micke <mikael.johanssonj@gmail.om>
2021-02-25 15:34:46 +0100

    asd

--- includes/config.php
+++ includes/config.php
@@ -9,4 +9,15 @@ $database = "testingSQL";
 $sql = mysqli_connect($host, $username, $password, $database) or die('Could not connect');
 
 
+
+//Select specific columns from DB
+$query = mysqli_query($sql, "SELECT * FROM tbl_articles");
+while($row = mysqli_fetch_assoc($query))
+{
+  $title = $row['title'];
+  $content = $row['content'];
+}
+
+
+
  ?>
