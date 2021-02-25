335c3fab4e863758f0565074376b77cd7a09759f
Micke <mikael.johanssonj@gmail.om>
2021-02-25 15:26:21 +0100

    asdaf

--- index.php
+++ index.php
@@ -68,7 +68,7 @@ include_once("includes/config.php");
                 <div class="row">
                     <div class="left-text col-lg-6 col-md-12 col-sm-12 col-xs-12"
                         data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
-                        <? php
+                        <?php
                         $query = mysqli_query($sql, "SELECT * FROM tbl_articles");
                         while($row = mysqli_fetch_assoc($query))
                         {
