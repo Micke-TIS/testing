137d85426d603bba96252e5d662de461dff64027
Micke <mikael.johanssonj@gmail.om>
2021-04-21 08:28:52 +0200

    s

--- includes/config.php
+++ includes/config.php
@@ -18,24 +18,4 @@ while($row = mysqli_fetch_assoc($query))
   $title = $row['title'];
   $content = $row['content'];
 }
-
-if(isset($_POST['save']))
-{
-	 $first_name = $_POST['first_name'];
-	 $last_name = $_POST['last_name'];
-	 $city_name = $_POST['city_name'];
-	 $email = $_POST['email'];
-	 $sql = "INSERT INTO employee (first_name,last_name,city_name,email)
-	 VALUES ('$first_name','$last_name','$city_name','$email')";
-	 if (mysqli_query($conn, $sql)) {
-		echo "New record created successfully !";
-	 } else {
-		echo "Error: " . $sql . "
-" . mysqli_error($conn);
-	 }
-	 mysqli_close($conn);
-}
-
-
-
  ?>
